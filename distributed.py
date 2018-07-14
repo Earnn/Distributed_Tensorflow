0dcc36af4a92bd7ad40160cfb346959722dfbc64
Earnn <earnlune@gmail.com>
2018-07-14 19:48:22 +0700

    a

--- distributed.py
+++ distributed.py
@@ -1,108 +1,77 @@
-import math
+# Notes:
+# You need to have the clusterone package installed (pip install tensorport)
+# Export logs and outputs to /logs, your data is in /data.
+
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+from clusterone import get_data_path, get_logs_path
 
-# Flags for defining the tf.train.ClusterSpec
-tf.app.flags.DEFINE_string("ps_hosts", "",
-                           "Comma-separated list of hostname:port pairs")
-tf.app.flags.DEFINE_string("worker_hosts", "",
-                           "Comma-separated list of hostname:port pairs")
 
-# Flags for defining the tf.train.Server
-tf.app.flags.DEFINE_string("job_name", "", "One of 'ps', 'worker'")
-tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
-tf.app.flags.DEFINE_integer("hidden_units", 100,
-                            "Number of units in the hidden layer of the NN")
-tf.app.flags.DEFINE_string("data_dir", "/tmp/mnist-data",
-                           "Directory for storing mnist data")
-tf.app.flags.DEFINE_integer("batch_size", 100, "Training batch size")
-
-FLAGS = tf.app.flags.FLAGS
-
-IMAGE_PIXELS = 28
-
-def main(_):
-    ps_hosts = FLAGS.ps_hosts.split(",")
-    worker_hosts = FLAGS.worker_hosts.split(",")
-    
-    # Create a cluster from the parameter server and worker hosts.
-    cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
-
-    # Create and start a server for the local task.
-    server = tf.train.Server(cluster,
-                             job_name=FLAGS.job_name,
-                             task_index=FLAGS.task_index)
+# Get the environment parameters for distributed TensorFlow
+try:
+    job_name = os.environ['JOB_NAME']
+    task_index = os.environ['TASK_INDEX']
+    ps_hosts = os.environ['PS_HOSTS']
+    worker_hosts = os.environ['WORKER_HOSTS']
+except: # we are not on TensorPort, assuming local, single node
+    task_index = 0
+    ps_hosts = None
+    worker_hosts = None
         
-        if FLAGS.job_name == "ps":
-server.join()
-    elif FLAGS.job_name == "worker":
-    
-    # Assigns ops to the local worker by default.
-    with tf.device(tf.train.replica_device_setter(
-                                                  worker_device="/job:worker/task:%d" % FLAGS.task_index,
-                                                  cluster=cluster)):
         
-        # Variables of the hidden layer
-        hid_w = tf.Variable(
-                            tf.truncated_normal([IMAGE_PIXELS * IMAGE_PIXELS, FLAGS.hidden_units],
-                                                stddev=1.0 / IMAGE_PIXELS), name="hid_w")
-                                hid_b = tf.Variable(tf.zeros([FLAGS.hidden_units]), name="hid_b")
-                                
-                                # Variables of the softmax layer
-                                sm_w = tf.Variable(
-                                                   tf.truncated_normal([FLAGS.hidden_units, 10],
-                                                                       stddev=1.0 / math.sqrt(FLAGS.hidden_units)),
-                                                   name="sm_w")
-                                    sm_b = tf.Variable(tf.zeros([10]), name="sm_b")
-                                    
-                                    x = tf.placeholder(tf.float32, [None, IMAGE_PIXELS * IMAGE_PIXELS])
-                                    y_ = tf.placeholder(tf.float32, [None, 10])
-                                    
-                                    hid_lin = tf.nn.xw_plus_b(x, hid_w, hid_b)
-                                    hid = tf.nn.relu(hid_lin)
-                                    
-                                    y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
-                                    loss = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
-                                    
-                                    global_step = tf.Variable(0)
-                                    
-                                    train_op = tf.train.AdagradOptimizer(0.01).minimize(
-                                                                                        loss, global_step=global_step)
-                                        
-                                        saver = tf.train.Saver()
-                                        summary_op = tf.summary.merge_all()
-                                        init_op = tf.initialize_all_variables()
+# This function defines the master, ClusterSpecs and device setters
+def device_and_target():
+    # If FLAGS.job_name is not set, we're running single-machine TensorFlow.
+    # Don't set a device.
+    if FLAGS.job_name is None:
+        print("Running single-machine training")
+        return (None, "")
 
-# Create a "supervisor", which oversees the training process.
-sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
-                         logdir="/tmp/train_logs",
-                         init_op=init_op,
-                         summary_op=summary_op,
-                         saver=saver,
-                         global_step=global_step,
-                         save_model_secs=600)
+    # Otherwise we're running distributed TensorFlow.
+    print("Running distributed training")
+    if FLAGS.task_index is None or FLAGS.task_index == "":
+        raise ValueError("Must specify an explicit `task_index`")
+    if FLAGS.ps_hosts is None or FLAGS.ps_hosts == "":
+        raise ValueError("Must specify an explicit `ps_hosts`")
+    if FLAGS.worker_hosts is None or FLAGS.worker_hosts == "":
+        raise ValueError("Must specify an explicit `worker_hosts`")
 
-mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    cluster_spec = tf.train.ClusterSpec({
+            "ps": FLAGS.ps_hosts.split(","),
+            "worker": FLAGS.worker_hosts.split(","),
+    })
+    server = tf.train.Server(
+            cluster_spec, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+    if FLAGS.job_name == "ps":
+        server.join()
 
-# The supervisor takes care of session initialization, restoring from
-# a checkpoint, and closing when done or an error occurs.
-with sv.managed_session(server.target) as sess:
-    # Loop until the supervisor shuts down or 1000000 steps have completed.
-    step = 0
-        while not sv.should_stop() and step < 1000000:
-            # Run a training step asynchronously.
-            # See `tf.train.SyncReplicasOptimizer` for additional details on how to
-            # perform *synchronous* training.
-            
-            batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
-        train_feed = {x: batch_xs, y_: batch_ys}
-        
-        _, step = sess.run([train_op, global_step], feed_dict=train_feed)
-        if step % 100 == 0:
-            print "Done step %d" % step
+    worker_device = "/job:worker/task:{}".format(FLAGS.task_index)
+    # The device setter will automatically place Variables ops on separate
+    # parameter servers (ps). The non-Variable ops will be placed on the workers.
+    return (
+            tf.train.replica_device_setter(
+                    worker_device=worker_device,
+                    cluster=cluster_spec),
+            server.target,
+    )
 
-# Ask for all the services to stop.
-sv.stop()
+    device, target = device_and_target()        
 
-if __name__ == "__main__":
-    tf.app.run()
+# Defining graph
+with tf.device(device):
+        #TODO define your graph here
+        ...
+
+#Defining the number of training steps
+hooks=[tf.train.StopAtStepHook(last_step=100000)]
+
+with tf.train.MonitoredTrainingSession(master=target,
+    is_chief=(FLAGS.task_index == 0),
+    checkpoint_dir=FLAGS.logs_dir,
+    hooks = hooks) as sess:
+
+    while not sess.should_stop():
+            # execute training step here (read data, feed_dict, session)
+            # TODO define training ops
+            data_batch = ...
+            feed_dict = {...}
+            loss, _ = sess.run(...)
