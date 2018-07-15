df557c92840d0b3ef85b79833d443ffd1b39d3e2
Earnn <earnlune@gmail.com>
2018-07-15 22:00:51 +0700

    a

--- dtf.py
+++ dtf.py
@@ -1,11 +1,32 @@
-import numpy as np
+# import numpy as np
+# import tensorflow as tf
+
+# x = tf.placeholder(tf.float32, 100)
+
+# mean = tf.reduce_mean(x)
+
+
+# with tf.Session() as sess:
+#     result = sess.run(mean, feed_dict={x: np.random.random(100)})
+#     print(result)
+
 import tensorflow as tf
 
-x = tf.placeholder(tf.float32, 100)
 
-mean = tf.reduce_mean(x)
+cluster = tf.train.ClusterSpec({"local": ["localhost:2222", "localhost:2223"]})
+
+x = tf.constant(2)
 
 
-with tf.Session() as sess:
-    result = sess.run(mean, feed_dict={x: np.random.random(100)})
+with tf.device("/job:local/task:1"):
+    y2 = x - 66
+
+with tf.device("/job:local/task:0"):
+    y1 = x + 300
+    y = y1 + y2
+
+
+with tf.Session("grpc://localhost:2222") as sess:
+    result = sess.run(y)
     print(result)
+    
