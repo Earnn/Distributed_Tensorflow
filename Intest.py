328ff2b75261332859e4a475d4c76c9d44e6f47c
Earnn <earnlune@gmail.com>
2018-07-28 22:13:58 +0700

    a

--- Intest.py
+++ Intest.py
@@ -8,13 +8,23 @@ line = "======================================================================"
 
 # parameter_servers = ["localhost:2222"]
 # workers = ["localhost:2223","localhost:2224"]
-parameter_servers = ["192.168.148.12:2222"]
-workers = ["192.168.148.12:2223","192.168.148.12:2224","192.168.148.12:2225"]
-cluster = tf.train.ClusterSpec({"ps": parameter_servers, "worker":workers})
+tf.app.flags.DEFINE_integer("num_gpus", 0, "Index of task")
 tf.app.flags.DEFINE_string("job_name", "", "'ps' / 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task")
+tf.app.flags.DEFINE_string("ps_hosts", 0, "Parameter server hosts")
+tf.app.flags.DEFINE_string("worker_hosts", 0, "Worker hosts")
+tf.app.flags.DEFINE_string("server_protocol", 0, "Server protocol")
 FLAGS = tf.app.flags.FLAGS
-server = tf.train.Server(cluster,job_name=FLAGS.job_name,task_index=FLAGS.task_index, protocol='grpc+verbs')
+
+
+
+# parameter_servers = ["192.168.148.12:2222"]
+parameter_servers = FLAGS.ps_hosts
+workers = FLAGS.worker_hosts
+# workers = ["192.168.148.12:2223","192.168.148.12:2224","192.168.148.12:2225"]
+cluster = tf.train.ClusterSpec({"ps": parameter_servers, "worker":workers})
+
+server = tf.train.Server(cluster,job_name=FLAGS.job_name,task_index=FLAGS.task_index, protocol=FLAGS.server_protocol)
 
 with tf.device('/cpu:0'):
     start_time = time.time()
